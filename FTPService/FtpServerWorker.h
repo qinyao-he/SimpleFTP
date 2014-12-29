23d24a3777a6ee4d9d00154fcd2fe70f928b8057
hqythu <hqythu@gmail.com>
2014-12-29 15:03:25 +0800

    add worker

---
+++ FTPService/FtpServerWorker.h
@@ -0,0 +1,17 @@
+﻿#ifndef FTPSERVERWORKER_H
+#define FTPSERVERWORKER_H
+
+#include "../TCPService/TcpSocket.h"
+
+
+class FtpServerWorker
+{
+public:
+	FtpServerWorker();
+	~FtpServerWorker();
+
+	void operator () (TcpSocket socket);
+};
+
+
+#endif // FTPSERVERWORKER_H
